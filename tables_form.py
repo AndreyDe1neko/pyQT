5246e123a08bd66250a9659daf41dce54eb1e02b
AndreyDe1neko <daynneki@gmail.com>
2023-11-01 15:44:32 +0200

    add graphic gor first example

--- tables_form.py
+++ tables_form.py
@@ -1,7 +1,9 @@
+import os
+
 import pandas as pd
 from PyQt5.QtGui import QTextDocument
 from PyQt5.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox, QTableWidget, QTableWidgetItem, QHeaderView, \
     QDesktopWidget, QPushButton, QMainWindow, QFileDialog, QHBoxLayout
 from PyQt5.QtCore import Qt, QUrl
@@ -9,6 +11,8 @@ from PyQt5 import QtGui, QtPrintSupport
 import pdfkit
 import plotly.express as px
 import plotly.graph_objects as go
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 table_translation_dict = {
     "mqtt_server": "Сервер MQTT",
@@ -137,7 +141,6 @@ class LazyLoadTableWidget(QTableWidget):
         scrollbar = self.verticalScrollBar()
         max_value = scrollbar.maximum()
         current_value = scrollbar.value()
-        print(current_value)
         # Якщо користувач долистав до кінця таблиці
         if current_value >= max_value:
             self.load_more_data()
@@ -173,9 +176,7 @@ class TablesWindow(QWidget):
         layout = QVBoxLayout(self)
         layout.setAlignment(Qt.AlignTop)
 
-        self.table_combobox = QComboBox(self)
-        self.table_combobox.addItems(self.table_names)
-        self.table_combobox.setStyleSheet("""
+        default_for_combobox = """
             QComboBox {
                 background-color: #2E2E2E;
                 color: #FFFFFF;
@@ -202,7 +203,30 @@ class TablesWindow(QWidget):
             {
             background-color: purple;
             }
-        """)
+        """
+
+        default_for_buttons = '''
+                    QPushButton {
+                        background-color: #732370;
+                        color: white;
+                        padding: 10px 20px;
+                        border-radius: 15px;
+                        font-size: 14px;
+                        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5); /* Додайте тінь */
+                    }
+                    QPushButton:hover {
+                        background-color: #9551a6;
+
+                        }
+                    QPushButton:pressed {
+                        background-color: #43185d;  
+                        border: 2px solid #9551a6;  
+                    }
+                    '''
+
+        self.table_combobox = QComboBox(self)
+        self.table_combobox.addItems(self.table_names)
+        self.table_combobox.setStyleSheet(default_for_combobox)
         self.table_combobox.setFixedWidth(200)
         self.table_combobox.currentIndexChanged.connect(self.on_combobox_change)
         layout.addWidget(self.table_combobox)
@@ -211,32 +235,44 @@ class TablesWindow(QWidget):
         layout.addWidget(self.table_widget)
         self.setLayout(layout)
 
+        self.power_bi_app_first = PowerBIApp_first(connection)
+        self.power_bi_app_first_graphic = PowerBIApp_first_graphic(connection)
+        self.power_bi_app_second = PowerBIApp_second(connection)
+        self.power_like_bi_app = PowerLikeBIApp(connection)
+
         self.report_like_BI_button = QPushButton('Показати', self)
         layout.addWidget(self.report_like_BI_button)
         self.report_like_BI_button.clicked.connect(self.report_like_BI_function)
-        self.report_like_BI_button.setStyleSheet('''
-            QPushButton {
-                background-color: #732370;
-                color: white;
-                padding: 10px 20px;
-                border-radius: 15px;
-                font-size: 14px;
-                box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5); /* Додайте тінь */
-            }
-            QPushButton:hover {
-                background-color: #9551a6;
+        self.report_like_BI_button.setStyleSheet(default_for_buttons)
 
-                }
-            QPushButton:pressed {
-                background-color: #43185d;  
-                border: 2px solid #9551a6;  
-            }
-            ''')
+        self.report_BI_button = QPushButton('Показати', self)
+        layout.addWidget(self.report_BI_button)
+        self.report_BI_button.clicked.connect(self.report_BI_function)
+        self.report_BI_button.setStyleSheet(default_for_buttons)
+
+        self.connected_stations_without_dublicate_button = QPushButton('Показати', self)
+        layout.addWidget(self.connected_stations_without_dublicate_button)
+        self.connected_stations_without_dublicate_button.clicked.connect(self.report_BI_second_function)
+        self.connected_stations_without_dublicate_button.setStyleSheet(default_for_buttons)
+
+        self.first_graphic_button = QPushButton('Показати', self)
+        layout.addWidget(self.first_graphic_button)
+        self.first_graphic_button.clicked.connect(self.report_graphic_Like_BI_function)
+        self.first_graphic_button.setStyleSheet(default_for_buttons)
+
+    def report_BI_function(self):
+        self.power_bi_app_first.show()
+
+    def report_graphic_Like_BI_function(self):
+        self.power_bi_app_first_graphic.show()
+
+    def report_BI_second_function(self):
+        self.power_bi_app_second.show()
 
     def report_like_BI_function(self):
-        if not self.power_bi_app:
-            self.power_bi_app = PowerBIApp(self.connection)
-        self.power_bi_app.show()
+        self.power_like_bi_app.show()
+
+
 
     def on_combobox_change(self):
         self.table_widget.clearContents()
@@ -252,7 +288,7 @@ class TablesWindow(QWidget):
     # self.table_combobox.currentIndexChanged.connect(self.on_combobox_change)
 
 
-class PowerBIApp(QMainWindow):
+class PowerLikeBIApp(QMainWindow):
     def __init__(self, connection):
         super().__init__()
         self.connection = connection
@@ -267,10 +303,9 @@ class PowerBIApp(QMainWindow):
 
         # Создаем HTML-код для таблицы
         cursor = self.connection.cursor()
-        sql_query = "SELECT * FROM connected_stations_list_view"
+        sql_query = "SELECT * FROM connected_stations_without_dublicate"
         cursor.execute(sql_query)
         data = cursor.fetchall()
-        print(data)
         html_content = f"""
         <!DOCTYPE html>
         <html lang="en">
@@ -374,7 +409,6 @@ class PowerBIApp(QMainWindow):
 
         self.setGeometry(100, 100, 800, 600)
         self.setWindowTitle('HTML Viewer')
-        self.show()
 
     def printToPdf(self, html_content):
         # Создаем QTextDocument из HTML-кода
@@ -406,107 +440,159 @@ class PowerBIApp(QMainWindow):
             pdfkit.from_string(html_content, file_path, configuration=config)
 
 
+class CustomWebEnginePage(QWebEnginePage):
+    def certificateError(self, error):
+        # Ignore SSL certificate errors
+        return True
 
 
+class PowerBIApp_second(QMainWindow):
+    def __init__(self, connection):
+        super().__init__()
+        self.setWindowTitle('Power BI Report')
+        self.setGeometry(100, 100, 800, 600)
+        self.initUI()
+
+    def initUI(self):
+        self.webview = QWebEngineView(self)
+        self.setCentralWidget(self.webview)
+
+        # Set custom web engine page to handle SSL certificate errors
+        page = CustomWebEnginePage(self.webview)
+        self.webview.setPage(page)
+
+        # URL of the Power BI report
+        report_url = "https://app.powerbi.com/groups/me/reports/3afd1bfa-a43c-42a6-90dd-b6c284878b1e/ReportSection?ctid=b42ffd57-19ac-4d2f-867f-e971c16b5159&experience=power-bi"
 
 
+        # Load the Power BI report in the QWebEngineView
+        self.webview.load(QUrl(report_url))
 
 
+class PowerBIApp_first(QMainWindow):
+    def __init__(self, connection):
+        super().__init__()
+        self.setWindowTitle('Power BI Report')
+        self.setGeometry(100, 100, 800, 600)
+        self.connection = connection
+        self.initUI()
+
+    def initUI(self):
+        self.webview = QWebEngineView(self)
+        self.setCentralWidget(self.webview)
+
+        # Set custom web engine page to handle SSL certificate errors
+        page = CustomWebEnginePage(self.webview)
+        self.webview.setPage(page)
+
+        # URL of the Power BI report
+        report_url = "https://app.powerbi.com/groups/me/reports/9abc14ba-e6cb-4f5e-9507-6fc776b81356/ReportSection?ctid=b42ffd57-19ac-4d2f-867f-e971c16b5159&pbi_source=shareVisual&visual=4872a1397275ad62d5d6&height=503.45&width=581.20&bookmarkGuid=b6ef4b18-ac65-423f-9b93-f8e5451ba06c"
+
+        # Load the Power BI report in the QWebEngineView
+        self.webview.load(QUrl(report_url))
 
 
+class PowerBIApp_first_graphic(QMainWindow):
+    def __init__(self, connection):
+        super().__init__()
+        self.setWindowTitle('Power BI Report')
+        self.setGeometry(100, 100, 800, 600)
+        self.connection = connection
 
+        # Створюємо головний контейнер для розміщення елементів у вікні
+        main_layout = QVBoxLayout()
 
+        # Додаємо QComboBox
+        self.comboBox = QComboBox()
+        self.comboBox.currentIndexChanged.connect(self.handle_combobox_change)
+        self.comboBox.setStyleSheet("""
+            QComboBox {
+                background-color: #2E2E2E;
+                color: #FFFFFF;
+                border: 1px solid #555555;
+                padding: 5px;
+                font-size: 16px;
+            }
+            QComboBox QAbstractItemView {
+                background-color: #2E2E2E;
+                color: #FFFFFF;
+                selection-background-color: #7a506f; 
+            }
+            QComboBox QAbstractItemView::item {
+                background-color: #2E2E2E;
+                color: #FFFFFF;
+            }
+            QComboBox QAbstractItemView::item:hover {
+                background-color: #7a506f; 
+            }
+            QComboBox::item:selected {
+                background-color: #4CAF50;
+            }
+            QComboBox::hover
+            {
+            background-color: purple;
+            }
+        """)
+        main_layout.addWidget(self.comboBox)
 
+        # Створюємо Figure та встановлюємо його розміри та стиль графіку
+        self.figure, self.ax = plt.subplots(figsize=(5, 4))
+        self.canvas = FigureCanvas(self.figure)
+        main_layout.addWidget(self.canvas)
 
+        # Створюємо центральний віджет та встановлюємо його головний контейнер
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
 
+        # Викликаємо метод для заповнення QComboBox початковими даними
+        self.populate_combobox()
 
-    # def __init__(self, connection, table_widget):
-    #     super().__init__()
-    #     self.connection = connection
-    #
-    #     self.table_widget = table_widget
-    #     self.setWindowTitle("Power BI-like App")
-    #     self.setGeometry(100, 100, 800, 600)
-    #
-    #     layout = QVBoxLayout()
-    #     self.web_view = QWebEngineView()
-    #
-    #     central_widget = QWidget()
-    #     central_layout = QVBoxLayout()
-    #     central_layout.addWidget(self.web_view)
-    #     central_widget.setLayout(central_layout)
-    #     layout.addWidget(central_widget)
-    #
-    #     self.setCentralWidget(central_widget)
-    #     self.load_data_and_plot()
-    #     self.export_button = QPushButton("Експорт в Excel")
-    #     self.export_button.clicked.connect(self.export_to_excel)
-    #
-    #     layout.addWidget(self.export_button)
-    #     self.export_button.show()  # Убедитесь, что кнопка видна
-    #
-    # def export_to_excel(self):
-    #     cursor = self.connection.cursor()
-    #     sql_query = "SELECT * FROM connected_stations_list_view"
-    #     cursor.execute(sql_query)
-    #     data = cursor.fetchall()
-    #     if data:
-    #         # Преобразуйте кортеж в DataFrame
-    #         df = pd.DataFrame(data, columns=['Location', 'Status', 'Parameter'])  # Замените названия столбцов на свои
-    #         excel_file_path = 'D:/my_projects/python/Tkinter/data.xlsx'
-    #         df.to_excel(excel_file_path, index=False)
-    #         print(f'Data exported to {excel_file_path}')
-    #     else:
-    #         print('No data to export')
-    #
-    # def load_data_and_plot(self):
-    #         # Load data (replace this with your data loading logic)
-    #     cursor = self.connection.cursor()
-    #     sql_query = "SELECT * FROM connected_stations_list_view"
-    #     cursor.execute(sql_query)
-    #     data = cursor.fetchall()
-    #
-    #     # Create a DataFrame from the retrieved data
-    #     df = pd.DataFrame(data, columns=['Location', 'Status', 'Parameter'])
-    #
-    #     # Create a Plotly table from the DataFrame
-    #     fig = go.Figure(data=[go.Table(header=dict(values=list(df.columns)),
-    #                                    cells=dict(values=[df.Location, df.Status, df.Parameter]))
-    #                           ])
-    #
-    #     # Save the Plotly table as an HTML file
-    #     html_file_path = 'D:/my_projects/python/Tkinter/plotly_chart.html'
-    #     fig.write_html(html_file_path, auto_open=False)
-    #
-    #     # Load the HTML file into the web widget
-    #     self.web_view.load(QUrl.fromLocalFile(html_file_path))
+    def populate_combobox(self):
+        cursor = self.connection.cursor()
+        sql_query_db_names = "SELECT DISTINCT Адреса FROM connected_stations_without_dublicate"
+        cursor.execute(sql_query_db_names)
+        adress_name = cursor.fetchall()
 
+        adress_names = [list(name)[0] for name in adress_name]
+        for name in adress_names:
+            self.comboBox.addItem(name)
 
+    def handle_combobox_change(self):
+        selected_address = self.comboBox.currentText()
 
-    # def load_data_and_plot(self):
-    #     # Load data (replace this with your data loading logic)
-    #     data = []
-    #     for row in range(self.table_widget.rowCount()):
-    #         row_data = []
-    #         for column in range(self.table_widget.columnCount()):
-    #             item = self.table_widget.item(row, column)
-    #             if item is not None:
-    #                 row_data.append(item.text())
-    #             else:
-    #                 row_data.append(None)
-    #         data.append(row_data)
-    #     print(data)
-    #     # Створити DataFrame з отриманих даних
-    #     column_names = [self.table_widget.horizontalHeaderItem(col).text() for col in
-    #                     range(self.table_widget.columnCount())]
-    #     print(column_names)
-    #     df = pd.DataFrame(data, columns=column_names)
-    #     #
-    #     # Створити графік Plotly з DataFrame
-    #     fig = px.bar(df, x=column_names[0], y=column_names[1], title='Reporting services')
-    #     #
-    #     # # Зберегти графік Plotly як HTML-файл
-    #     fig.write_html('D:/my_projects/python/Tkinter/plotly_chart.html', auto_open=False)
-    #
-    #     # Завантажити HTML-файл у веб-віджет
-    #     self.web_view.load(QUrl.fromLocalFile('D:/my_projects/python/Tkinter/plotly_chart.html'))
+        cursor = self.connection.cursor()
+        sql_query = f"SELECT \"Вимір\", MIN(\"Величина\"), MAX(\"Величина\"), AVG(\"Величина\") FROM station_measurment_time_view WHERE \"Адреса\" = '{selected_address}' GROUP BY \"Вимір\""
+        cursor.execute(sql_query)
+        results = cursor.fetchall()
+        # Розділіть результати на назви величин, мінімальні, максимальні та середні значення
+        measurement_names = [row[0] for row in results]
+        # print(measurement_names)
+        min_values = [row[1] for row in results]
+        max_values = [row[2] for row in results]
+        avg_values = [row[3] for row in results]
+        # print(max_values)
+        # print(min_values)
+        # print(avg_values)
+
+        # Очистимо попередні дані з графіку та відобразимо нові дані
+        self.ax.clear()
+
+        # Побудова графіку для кожної величини
+        for i, name in enumerate(measurement_names):
+            min_val = min_values[i]
+            max_val = max_values[i]
+            avg_val = avg_values[i]
+
+            self.ax.barh(f"{name} (min)", min_val, color='r', label='Мінімальне значення')
+            self.ax.barh(f"{name} (max)", max_val, color='g', left=0, label='Максимальне значення')
+            self.ax.barh(f"{name} (avg)", avg_val, color='b', left=0, label='Середнє значення')
+
+        self.ax.set_xlabel('Значення')
+        self.ax.set_ylabel('Величина вимірювання')
+        self.ax.set_title(f'Мінімальні, максимальні та середні значення для адреси: {selected_address}')
+        self.ax.legend()
+        self.ax.grid(True)
+
+        # Оновимо графік на віджеті FigureCanvas
+        self.canvas.draw()
